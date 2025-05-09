30f3ebba9b40cc634308b53e753b0f9d79d4174a
laurhakob <laurhakob@gmail.com>
2025-05-09 23:51:46 +0400

    fully setup convex auth with github, google, and sign up with password

---
+++ src/middleware.ts
@@ -0,0 +1,54 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"]);
+
+export default convexAuthNextjsMiddleware(async (request) => {
+  // Use await to properly wait for the Promise to resolve
+  const isAuthenticated = await isAuthenticatedNextjs();
+
+  if (!isPublicPage(request) && !isAuthenticated) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+
+  if (isPublicPage(request) && isAuthenticated) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+});
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+
+
+// import {
+//   createRouteMatcher,
+//   convexAuthNextjsMiddleware,
+//   nextjsMiddlewareRedirect,
+// } from "@convex-dev/auth/nextjs/server";
+// import { isAuthenticated } from "./convex/auth";
+
+// const isPublicPage = createRouteMatcher(["/signin"]);
+
+// export default convexAuthNextjsMiddleware((request) => {
+//   if (!isPublicPage(request) && !isAuthenticated) {
+//     return nextjsMiddlewareRedirect(request, "/auth");
+//   }
+
+//   if (isPublicPage(request) && isAuthenticated) {
+//     return nextjsMiddlewareRedirect(request, "/");
+//   }
+// });
+
+// export const config = {
+//   // The following matcher runs middleware on all routes
+//   // except static assets.
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
